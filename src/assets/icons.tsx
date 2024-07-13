294fcc7bdd89f1e8c71d8e48efd6d11122d92ad8
= <=>
2024-07-13 15:30:03 -0700

    start creating waveform button icons

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -39,3 +39,51 @@ export function StopIcon() {
     </svg>
   );
 }
+
+export function SineWaveIcon() {
+  return (
+    <svg
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+      stroke-linejoin="round"
+      stroke-miterlimit="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="m11.998 2c5.517 0 9.997 4.48 9.997 9.998 0 5.517-4.48 9.997-9.997 9.997-5.518 0-9.998-4.48-9.998-9.997 0-5.518 4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 8.498s3.808 8.497 8.498 8.497 8.497-3.807 8.497-8.497-3.807-8.498-8.497-8.498z"
+        fill-rule="nonzero"
+      />
+    </svg>
+  );
+}
+
+export function SquareWaveIcon() {
+  return (
+    <svg
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+      stroke-linejoin="round"
+      stroke-miterlimit="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="m21 4c0-.478-.379-1-1-1h-16c-.62 0-1 .519-1 1v16c0 .621.52 1 1 1h16c.478 0 1-.379 1-1zm-16.5.5h15v15h-15z"
+        fill-rule="nonzero"
+      />
+    </svg>
+  );
+}
+
+// export function SawWaveIcon() {
+//   return (
+//
+//   );
+// }
+//
+// export function TriangleWaveIcon() {
+//   return (
+//
+//   );
+// }
