3c3864f4d9e058becb023f57284575204dd28535
= <=>
2024-07-15 15:44:55 -0700

    Add name of app to top left

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Wrapper from "./components/Wrapper";
 export default function App() {
   return (
     <div className="font-sans text-white bg-zinc-900">
+      <h1 className="absolute left-4 top-2 text-neutral-400 italic font-semibold text-3xl">
+        SEQLR
+      </h1>
       <Nav />
       <Wrapper>
         <main className="min-h-custom flex flex-col gap-3 justify-center items-center">
