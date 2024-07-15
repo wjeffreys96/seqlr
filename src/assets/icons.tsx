a63f30d54f0f5246f2de63ecd869deff32ae8e48
= <=>
2024-07-15 14:59:40 -0700

    add icons for waveform buttons

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -43,17 +43,20 @@ export function StopIcon() {
 export function SineWaveIcon() {
   return (
     <svg
-      clip-rule="evenodd"
-      fill-rule="evenodd"
-      stroke-linejoin="round"
-      stroke-miterlimit="2"
-      viewBox="0 0 24 24"
+      width="15px"
+      height="15px"
+      viewBox="0 0 512 512"
+      version="1.1"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="m11.998 2c5.517 0 9.997 4.48 9.997 9.998 0 5.517-4.48 9.997-9.997 9.997-5.518 0-9.998-4.48-9.998-9.997 0-5.518 4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 8.498s3.808 8.497 8.498 8.497 8.497-3.807 8.497-8.497-3.807-8.498-8.497-8.498z"
-        fill-rule="nonzero"
-      />
+      <g id="Page-1" stroke="none" strokeWidth="1" fillRule="evenodd">
+        <g id="icon" transform="translate(42.666667, 42.666667)">
+          <path
+            d="M213.333333,3.55271368e-14 C331.15408,3.55271368e-14 426.666667,95.5125867 426.666667,213.333333 C426.666667,331.15408 331.15408,426.666667 213.333333,426.666667 C95.5125867,426.666667 3.55271368e-14,331.15408 3.55271368e-14,213.333333 C3.55271368e-14,95.5125867 95.5125867,3.55271368e-14 213.333333,3.55271368e-14 Z"
+            id="Combined-Shape"
+          ></path>
+        </g>
+      </g>
     </svg>
   );
 }
@@ -61,29 +64,44 @@ export function SineWaveIcon() {
 export function SquareWaveIcon() {
   return (
     <svg
-      clip-rule="evenodd"
-      fill-rule="evenodd"
-      stroke-linejoin="round"
-      stroke-miterlimit="2"
+      width="15px"
+      height="15px"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="m21 4c0-.478-.379-1-1-1h-16c-.62 0-1 .519-1 1v16c0 .621.52 1 1 1h16c.478 0 1-.379 1-1zm-16.5.5h15v15h-15z"
-        fill-rule="nonzero"
-      />
+      <path d="M18.6 3H5.4A2.4 2.4 0 0 0 3 5.4v13.2A2.4 2.4 0 0 0 5.4 21h13.2a2.4 2.4 0 0 0 2.4-2.4V5.4A2.4 2.4 0 0 0 18.6 3Z" />
     </svg>
   );
 }
 
-// export function SawWaveIcon() {
-//   return (
-//
-//   );
-// }
-//
-// export function TriangleWaveIcon() {
-//   return (
-//
-//   );
-// }
+export function SawWaveIcon() {
+  return (
+    <svg
+      width="15px"
+      height="15px"
+      viewBox="0 0 256 256"
+      id="Flat"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M236,139.31348,139.31348,236a16.01779,16.01779,0,0,1-22.627,0L20,139.31348a16.01779,16.01779,0,0,1,0-22.627L116.68652,20a16.01779,16.01779,0,0,1,22.627,0L236,116.68652A16.01779,16.01779,0,0,1,236,139.31348Z" />
+    </svg>
+  );
+}
+
+export function TriangleWaveIcon() {
+  return (
+    <svg
+      width="15px"
+      height="15px"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 1.25a2.101 2.101 0 00-1.785.996l.64.392-.642-.388-5.675 9.373-.006.01a2.065 2.065 0 00.751 2.832c.314.183.67.281 1.034.285h11.366a2.101 2.101 0 001.791-1.045 2.064 2.064 0 00-.006-2.072L9.788 2.25l-.003-.004A2.084 2.084 0 008 1.25z"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
