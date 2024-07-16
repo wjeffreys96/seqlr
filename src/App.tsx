ae0f418633250e1a24ac57a3a7e3319dd591333b
= <=>
2024-07-16 14:14:35 -0700

    style app name

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Wrapper from "./components/Wrapper";
 export default function App() {
   return (
     <div className="font-sans text-white bg-zinc-900">
-      <h1 className="absolute left-4 top-2 text-neutral-400 italic font-semibold text-3xl">
+      <h1 className="w-32 h-12 text-center absolute left-6 top-2 italic bg-gradient-to-r from-neutral-500 via-neutral-300 via-60% to-neutral-400 text-transparent bg-clip-text font-bold text-4xl">
         SEQLR
       </h1>
       <Nav />
