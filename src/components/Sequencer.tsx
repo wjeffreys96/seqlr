9a40dd70ae364b39d7545492b53f52e943f279f4
= <=>
2024-08-15 10:13:35 -0700

    implement react-window for sequencers

--- src/components/Sequencer.tsx
+++ src/components/Sequencer.tsx
@@ -1,13 +1,14 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { cn } from "../utils/cn.ts";
 import { audioCtx } from "../AudioContext.ctx.tsx";
 import type { AudioContextType, SequencerObject } from "../@types/AudioContext";
 import KnobModule from "./KnobModule";
 import NodeList from "./NodeList.tsx";
+import { FixedSizeList as List, ListChildComponentProps } from "react-window";
 
 export default function Sequencer() {
   const actx = useContext<AudioContextType>(audioCtx);
-  const { state, dispatch } = actx;
+  const { state } = actx;
   const seqRefArr = useRef<(HTMLDivElement | null)[]>([]);
   const globXScrollRef = useRef<HTMLDivElement | null>(null);
   const [scrollWidth, setScrollWidth] = useState<number>(
@@ -36,60 +37,75 @@ export default function Sequencer() {
     });
   };
 
-  if (state && dispatch) {
-    const {
-      currentNote,
-      masterPlaying,
-      globSeqArr,
-      nodeCount,
-    }: {
-      currentNote: number;
-      masterPlaying: boolean;
-      globSeqArr: SequencerObject[];
-      nodeCount: number;
-    } = state;
+  const itemKey = useCallback((index: number, arr: SequencerObject[]) => {
+    const item = arr[index];
+    return `gsak-${item.id}-${index}`;
+  }, []);
 
-    if (globSeqArr.length > 0) {
-      return (
-        <>
-          {seqRefArr.current[0] && (
-            <div className="flex flex-col justify-center pb-[1px] gap-4 mx-1.5 px-4 lg:h-4 lg:bg-inherit bg-neutral-800 rounded-lg h-8 ">
-              <div
-                ref={globXScrollRef}
-                onScroll={handleXScroll}
-                style={{ maxWidth: `${scrollContainerWidth}px` }}
-                className="overflow-x-scroll scrollbar-thin scrollbar-track-neutral-700 w-full h-full"
-              >
-                <div
-                  className="h-[1px]"
-                  style={{ width: `${scrollWidth}px` }}
-                />
-              </div>
+  const {
+    currentNote,
+    masterPlaying,
+    globSeqArr,
+    nodeCount,
+  }: {
+    currentNote: number;
+    masterPlaying: boolean;
+    globSeqArr: SequencerObject[];
+    nodeCount: number;
+  } = state!;
+
+  const SequencerList = useCallback(
+    ({ style, index }: ListChildComponentProps) => {
+      if (globSeqArr.length > 1) {
+        return (
+          <div
+            style={style}
+            className="flex flex-col gap-4 mx-1.5 my-2 bg-neutral-800 p-4 rounded-lg border border-neutral-700"
+          >
+            <KnobModule outerIndex={index} />
+            <div className={cn("flex bg-neutral-900 p-3 rounded-xl ")}>
+              <NodeList
+                arr={globSeqArr[index]}
+                outerIndex={index}
+                masterPlaying={masterPlaying}
+                nodeCount={nodeCount}
+                currentNote={currentNote}
+              />
             </div>
-          )}
-          {globSeqArr.map((arr, outerIndex) => {
-            return (
-              <div
-                key={"gnak" + outerIndex}
-                className="flex flex-col gap-4 mx-1.5 my-2 bg-neutral-800 p-4 rounded-lg border border-neutral-700"
-              >
-                <KnobModule outerIndex={outerIndex} />
-                <div className={cn("flex bg-neutral-900 p-3 rounded-xl ")}>
-                  <NodeList
-                    arr={arr}
-                    outerIndex={outerIndex}
-                    masterPlaying={masterPlaying}
-                    nodeCount={nodeCount}
-                    currentNote={currentNote}
-                  />
-                </div>
-              </div>
-            );
-          })}
-        </>
-      );
-    }
-  } else {
-    throw new Error("actx is undefined");
+          </div>
+        );
+      }
+    },
+    [globSeqArr, currentNote, masterPlaying],
+  );
+  if (globSeqArr.length > 0) {
+    return (
+      <>
+        {seqRefArr.current[0] && (
+          <div className="flex flex-col justify-center pb-[1px] gap-4 mx-1.5 px-4 lg:h-4 lg:bg-inherit bg-neutral-800 rounded-lg h-8 ">
+            <div
+              ref={globXScrollRef}
+              onScroll={handleXScroll}
+              style={{ maxWidth: `${scrollContainerWidth}px` }}
+              className="overflow-x-scroll scrollbar-thin scrollbar-track-neutral-700 w-full h-full"
+            >
+              <div className="h-[1px]" style={{ width: `${scrollWidth}px` }} />
+            </div>
+          </div>
+        )}
+
+        <List
+          className="scrollbar-thin"
+          height={880}
+          width={1910}
+          itemCount={globSeqArr.length}
+          itemSize={210}
+          itemData={globSeqArr}
+          itemKey={itemKey}
+        >
+          {SequencerList}
+        </List>
+      </>
+    );
   }
 }
